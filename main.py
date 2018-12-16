7c7067946e2129536991b8aa67172c39cb99c427
Sherm4nLC <german.lopezcampos@gmail.com>
2018-12-16 14:08:19 +0000

    main

--- main.py
+++ main.py
@@ -2,9 +2,24 @@
 
 import pandas as pd
 import tensorflow as tf
+from datetime import datetime as dt
 
 '''
 The intention is to create the submission files
 starting from the provided dataset
 '''
 
+class ProcessTitanic(object):
+	def __init__(self):
+		self.init_time = dt.now()
+		self.data = None
+		pass
+
+	def load_data(self):
+		self.data = 1
+
+if __name__=="__main__":
+	pt = ProcessTitanic()
+	print(pt.init_time)
+	pt.load_data()
+	print(pt.data)
